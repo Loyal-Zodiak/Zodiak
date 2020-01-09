520f927cec69d2dc6cb67a0ce3ab4ecd7c802deb
Amil Hasbala <amilhasbala@gmail.com>
2020-01-09 20:54:24 +0700

    github login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,44 @@
+const {OAuth2Client} = require('google-auth-library'),
+  axios = require('axios')
+
 class UserController {
   static googleLogin(req, res, next) {
-    res.send('login')
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const client = new OAuth2Client(clientId)
+    client.verifyIdToken({
+        idToken: req.body.idToken,
+        audience: clientId
+    })
+      .then(ticket => {
+        const googlePayload = ticket.getPayload()
+        // lanjut ke JOVI
+        console.log(googlePayload.name)
+        res.status(200).json({name: googlePayload.name})
+      })
+      .catch(next)
+  }
+  static githubLogin (req, res, next) {
+    let githubToken = null
+    axios({
+      method: 'post',
+      url: `https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${req.query.code}`
+    })
+      .then(({data}) => {
+        githubToken = data.slice(13, 53)
+        return axios({
+          url: 'https://api.github.com/user',
+          method: 'get',
+          headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`
+          }
+        })
+      })
+      .then(({data}) => {
+        // lanjut ke JOVI
+        console.log(data.login)
+        res.status(200).json({name: data.login})
+      })
+      .catch(next)
   }
 }
 
