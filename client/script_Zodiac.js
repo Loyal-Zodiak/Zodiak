c5723328425aa4139ca69accda9fe78c07fec5ff
Okka Linardi <okka.linardi@gmail.com>
2020-01-10 13:40:23 +0700

    Fixed gambar horoscope di translate dan menghapus yang tidak perlu di script_Zodiac

--- client/script_Zodiac.js
+++ client/script_Zodiac.js
@@ -33,6 +33,7 @@ function getToday(sign){
         success: function (result) {
           console.log(result)
           zodiak = result
+          zodiak.name = sign
           // console.log(result)
             $('#field-ramalan').append(`
             <div id="yesterday-btn" class="btn btn-white" style="background-color: whitesmoke;">Yesterday</div>
@@ -67,27 +68,6 @@ function getToday(sign){
               </div>
                 `).hide().fadeIn(400)
 
-            $('.horoscopeData').html(`
-            <div class="card" style="width: 18rem;">
-            <img src="https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_${sign.toLowerCase()}.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${sign}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${result.date_range}</h6>
-              <p class="card-text">${result.description}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><h6>compatibility: ${result.compatibility}</h6></li>
-              <li class="list-group-item"><h6>mood: ${result.mood}</h6></li>
-              <li class="list-group-item"><h6>color: ${result.color}</h6></li>
-              <li class="list-group-item"><h6>lucky number: ${result.lucky_number}</h6></li>
-              <li class="list-group-item"><h6>lucky time: ${result.lucky_time}</h6></li>
-            </ul>
-            <div class="card-body">
-                <button type="button" class="btn btn-outline-primary" style="width:100%;">Translate</button>
-            </div>
-          </div>
-
-                `).hide().fadeIn(400)
         }
     })
 }
@@ -99,7 +79,7 @@ function getTomorrow(sign){
     type: 'post', // Untuk img src dan h5 , kata "taurus" diganti ke => ${parameter}
     success: function (result) {
       zodiak = result
-
+      zodiak.name = sign
           $('#field-ramalan').append(`
           <div id="yesterday-btn" class="btn btn-white" style="background-color: whitesmoke;">Yesterday</div>
           <div id="today-btn" class="btn btn-white" style="background-color: whitesmoke;">Today</div>
@@ -145,6 +125,7 @@ function getYesterday(sign){
         type: 'post', // Untuk img src dan h5 , kata "taurus" diganti ke => ${parameter}
         success: function (result) {
           zodiak = result
+          zodiak.name = sign
           $('#field-ramalan').append(`
           <div id="yesterday-btn" class="btn btn-white" style="background-color: whitesmoke;">Yesterday</div>
           <div id="today-btn" class="btn btn-white" style="background-color: whitesmoke;">Today</div>
@@ -173,30 +154,10 @@ function getYesterday(sign){
                     </tr>
                   </tbody>
                 </table>
-                <a id="btn-translate" href="#" class="btn btn-primary">Bahasa Indonesia</a>
+                <a id="btn-translate" href="#" class="btn btn-primary">Translate</a>
               </div>
               `).fadeIn(400)
 
-            $('.horoscopeData').html(`
-            <div class="card" style="width: 18rem;">
-            <img src="https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_${sign.toLowerCase()}.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Taurus</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${result.date_range}</h6>
-              <p class="card-text">${result.description}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><h6>compatibility: ${result.compatibility}</h6></li>
-              <li class="list-group-item"><h6>mood: ${result.mood}</h6></li>
-              <li class="list-group-item"><h6>color: ${result.color}</h6></li>
-              <li class="list-group-item"><h6>lucky number: ${result.lucky_number}</h6></li>
-              <li class="list-group-item"><h6>lucky time: ${result.lucky_time}</h6></li>
-            </ul>
-            <div class="card-body">
-                <button type="button" class="btn btn-outline-primary" style="width:100%;">Translate</button>
-
-            </div>
-              `).hide().fadeIn(400)
         }
     })
 }
