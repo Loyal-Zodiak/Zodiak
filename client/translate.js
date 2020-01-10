ea8ded82e104cc8405a2a7026dace83f8c36e5a8
dzakki <dzakkiaz7@gmail.com>
2020-01-10 10:44:43 +0700

    merge fitur zodiak to translate

--- client/translate.js
+++ client/translate.js
@@ -53,9 +53,9 @@ class Translation {
             alert('please, choose language fisrt');
         }else{
             const text = ['date range', 'current date', "description", "compatibility", "mood", "color", "lucky_number", "lucky time"]
-            for (const key in example) {
-                if (example.hasOwnProperty(key)) {
-                    text.push(example[key]);
+            for (const key in zodiak) {
+                if (zodiak.hasOwnProperty(key)) {
+                    text.push(zodiak[key]);
                 }
             }
             Translation.$translate({
@@ -64,6 +64,7 @@ class Translation {
             })
             .done(({translation}) => {
                 Translation.generateHtmlOfTranslate(translation)
+                $('#modal-language').modal('hide')
             })
             .fail(errs => {
                 console.log(errs);
@@ -72,34 +73,49 @@ class Translation {
     }
 
     static generateHtmlOfTranslate(arrays) {
-        const elHoroscopeData = $('.horoscopeData')
+        const elFieldRamalan = $('#field-ramalan')
+        elFieldRamalan.empty()
         let html = `
-            <div class="card" id="horoscope">
-                <img src="https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_${arrays[11].translatedText.toLowerCase()}.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Taurus</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${arrays[11].translatedText}</h6>
+            <div id="yesterday-btn" class="btn btn-white" style="background-color: whitesmoke;">Yesterday</div>
+            <div id="today-btn" class="btn btn-white" style="background-color: whitesmoke;">Today</div>
+            <div id="tomorrow-btn" class="btn btn-white" style="background-color: whitesmoke;">Tomorrow</div>
+            <div class="card m-5 mx-auto" style="width: 40rem; background-color : rgba(30, 36, 41, 0.363) ;" height="400px">
+                <img src="https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_${zodiak.compatibility.toLowerCase()}.png" class="card-img-top">
+                <div class="card-body text-white" style="font-size: 25px;">
                 <p class="card-text">${arrays[10].translatedText}</p>
+                <table class="table table-borderless text-white table-responsive">
+                    <thead>
+                        <tr>
+                            <th scope="col">Compabilty</th>
+                            <th scope="col">Mood</th>
+                            <th scope="col">Colour</th>
+                            <th scope="col">Lucky No.</th>
+                            <th scope="col">Lucky Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>${arrays[11].translatedText}</td>
+                        <td>${arrays[12].translatedText}</td>
+                        <td>${arrays[13].translatedText}</td>
+                        <td>${arrays[14].translatedText}</td>
+                        <td>${arrays[15].translatedText}</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <a id="btn-translate" href="#" class="btn btn-primary">Translate</a>
                 </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item"><h6>${arrays[3].translatedText}: ${arrays[11].translatedText}</h6></li>
-                <li class="list-group-item"><h6>${arrays[4].translatedText}: ${arrays[12].translatedText}</h6></li>
-                <li class="list-group-item"><h6>${arrays[5].translatedText}: ${arrays[13].translatedText}</h6></li>
-                <li class="list-group-item"><h6>${arrays[6].translatedText}: ${arrays[14].translatedText}</h6></li>
-                <li class="list-group-item"><h6>${arrays[7].translatedText}: ${arrays[15].translatedText}</h6></li>
-                </ul>
-                <div class="card-body">
-                    <button type="button" class="btn btn-outline-primary" style="width:100%;">Translate</button>
-                </div>
-          </div>
+            </div>
         `
-        elHoroscopeData.append(html)
-        // elHoroscopeData.show('slow')
+        elFieldRamalan.append(html).hide().show('slow')
     }
 }
 
 $(document).ready(function () {
     Translation.generateSelectLanguage()
-    $(document).on('click', '#translate', Translation.doTranslate)
+    $(document).on('click', '#doTranslate', Translation.doTranslate)
+    $(document).on('click', '#btn-translate', function (e) {
+        $('#modal-language').modal('show')
+    })
 });
 
