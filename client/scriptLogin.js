ec98fcc5903afa8ce300092144836d62791e446f
Amil Hasbala <amilhasbala@gmail.com>
2020-01-10 10:25:49 +0700

    err handling jwt

--- client/scriptLogin.js
+++ client/scriptLogin.js
@@ -21,6 +21,7 @@ function onSignIn(googleUser) {
 
 // log out
 function signOut() {
+  localStorage.clear();
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function() {
     console.log("User signed out.");
